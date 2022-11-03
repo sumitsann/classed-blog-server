10a3b61cf49783a9b49e44b2365cd5d77ceec26c
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-11-03 11:19:28 -0500

    Updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const resolvers = require("./schemas/resolvers/index");
 const Blog = require("./models/Blog");
 const { MONGODB } = require("./config.js");
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
